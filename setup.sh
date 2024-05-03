2f1bcb20d2f3ab147d02b784f3ac4787d0752392
Kane Xu <xu.hong@northeastern.edu>
2024-05-03 19:18:15 -0400

    5/3 1

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"                                                                                       
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install node
-nvm use node
+nvm install 20
+nvm use 20
 npm install --global yarn
 yarn global add @vue/cli-service --save-dev
 echo "export PATH=\"\$PATH:$(yarn global bin)\"" >> ~/.bashrc && source ~/.bashrc
